bbb414003d16ebfb03d962200ba1efba8e0ba292
exzgit <officialbangezz@gmail.com>
2025-05-03 03:31:14 +0700

    Create a default-page that will be displayed if the user has not logged in,  and create a login & register form.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { AppComponent } from './app.component';
+import { DefaultPageComponent } from './default-page/default-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DefaultPageComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
+];
